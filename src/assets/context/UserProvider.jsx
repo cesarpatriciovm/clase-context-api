7616791b123a0332fad5b30174ccb06109569b76
cesarpatriciovm <cesarvalladaresmilla@gmail.com>
2025-01-27 15:47:26 -0300

    actualización

--- src/assets/context/UserProvider.jsx
+++ src/assets/context/UserProvider.jsx
@@ -1,8 +1,19 @@
 import { UserContext } from "./UserContext"
 
+const user = {
+  name: "John Doe",
+  email: "cesar@gmail.com",
+  avatar: "https://cdn.icon-icons.com/icons2/2643/PNG/512",
+  role: "admin",
+  phone: "1234567890",
+  address: "1234 Main St",
+  city: "Springfield",
+}
+
+// eslint-disable-next-line react/prop-types
 function UserProvider({ children }) {
   return (
-    <UserContext.Provider value={{ name: "Mi nombre es John Doe" }}>
+    <UserContext.Provider value={{ name: "Mi nombre es John Doe", user }}>
       {children}
     </UserContext.Provider>
   )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,14 @@ import { UserContext } from "../assets/context/UserContext"
 import { useContext } from 'react'
 
 function HomePage() {
-  const { name } = useContext(UserContext)
+  const { name, user } = useContext(UserContext)
   return (
     <>
-      <div className="text-center ">HomePage</div>
-      <span className="">{name}</span>
+      <div className="text-center m-5">HomePage</div>
+      <div className="flex flex-col items-center">
+        <span className="">{name}</span>
+        <span className="p-2">email: {user.email}</span>
+      </div>
     </>
   )
 }
